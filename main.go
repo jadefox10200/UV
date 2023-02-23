1dbafdf2ed36cdc4427fe91cacb4c7b92bcfb1e5
jadefox10200 <jadefox10200@gmail.com>
2023-02-22 19:53:41 -0800

    Restructered the code per some directions on slack. This is mainly so only one goroutine is running for each sensor. This is designed to handle the panic memory issue. Also added a memStats ticker every 5 seconds

--- main.go
+++ main.go
@@ -4,11 +4,9 @@ import (
 	"container/list"
 	"fmt"
 	"machine"
-	"runtime/debug"
+	"runtime"
 	"time"
 )
-
-//TODO: make a toml file for param uploads on board start up. possible?
 
 var maxDiff int64 = 300
 var qLenMax = 3
@@ -79,7 +77,7 @@ func NewQueue() Queue {
 func compare(value *list.Element, reference int64) bool {
 	//type assertion of the interface type list.Element any
 	diff := reference - value.Value.(int64)
-	//if the difference between the too is too great, return that they don't match
+	//if the difference between the two is too great, return that they don't match
 	if diff > maxDiff || diff < -maxDiff {
 		fmt.Printf("Wrong Diff: %v", diff)
 		return false
@@ -103,24 +101,22 @@ func blinky() {
 	}
 }
 
-func (q Queue) NewProcess() {
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-	t := time.Now()
+func (q Queue) InfeedSensor() {
 
 	for {
+		//wait for the sensor to turn on:
+		<-q.InputOn
+		t := time.Now()
+		ticker := time.NewTicker(time.Second)
 		select {
 		case <-q.InputOff:
 			//if the counter is too small we assume we didn't see a sheet:
 			diff := time.Since(t)
 			if diff.Milliseconds() < q.MinMilli {
 				fmt.Println("saw something small")
-				debug.FreeOSMemory()
-				return
+				ticker.Stop()
+				continue
 			}
-
-			//we got a sheet and so should reset the time bomb:
-			// q.Ticker.Reset(time.Second * 3)
 
 			//as the counter was larger than 1, we send the duration onto the queue:
 			q.Q.PushFront(diff.Milliseconds())
@@ -130,7 +126,7 @@ func (q Queue) NewProcess() {
 				return
 			}
 			fmt.Printf("added dur: %v; Q-len: %v\n", diff.Milliseconds(), q.Q.Len())
-			return
+			ticker.Stop()
 		case <-ticker.C:
 			diff := time.Since(t)
 			fmt.Printf("Milliseconds: %v\n", diff.Milliseconds())
@@ -139,47 +135,43 @@ func (q Queue) NewProcess() {
 				q.Kill <- true
 				return
 			}
-
 		}
 	}
 }
 
-func (q Queue) EndProcess() {
-
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-	t := time.Now()
+func (q Queue) OutFeedSensor() {
 
 	for {
+		//wait for the sensor to turn on:
+		<-q.OutputOn
+		//once on, capture the time and start a ticker:
+		t := time.Now()
+		ticker := time.NewTicker(time.Second)
 		select {
+		//sensor turns off, stop the ticker, check the length, add to Q:
 		case <-q.OutputOff:
+			ticker.Stop()
 			diff := time.Since(t)
-
 			//if the counter is too small we assume we didn't see a sheet:
 			if diff.Milliseconds() < q.MinMilli {
 				fmt.Printf("Saw something small on output\n")
-				debug.FreeOSMemory()
-				return
+				continue
 			}
-
-			//we got a sheet and so should reset the time bomb:
-			//q.Ticker.Reset(time.Second * 3)
-
 			if q.Q.Len() == 0 {
 				fmt.Printf("nothing in Q but output got a sheet!\n")
-				return
+				continue
 			}
+
 			//get the first element from the queue:
 			el := q.Q.Front()
 			if compare(el, diff.Milliseconds()) {
-				fmt.Println("Got the right one")
+				println("Got the right one")
 				q.Q.Remove(el)
 				fmt.Printf("removed an el. Q Len= %v\n", q.Q.Len())
 			} else {
-				fmt.Println("They didn't match, something wrong!")
+				println("They didn't match, something wrong!")
 				q.Kill <- true
 			}
-			return
 		case <-ticker.C:
 			diff := time.Since(t)
 			fmt.Printf("Milliseconds: %v\n", diff.Milliseconds())
@@ -189,6 +181,7 @@ func (q Queue) EndProcess() {
 			}
 
 		}
+
 	}
 }
 
@@ -196,55 +189,34 @@ func main() {
 
 	time.Sleep(5 * time.Second)
 	fmt.Println("run init")
-
-	fmt.Println("states assigned")
 	//set up channels:
 	//a ticker must be activated seperately:
 	q := NewQueue()
 	q.MinMilli = 300
 	q.MaxMilli = 4000
-	//simulating sheets entering the UV tunnel:
-	go func() {
-		// for {
-		// 	time.Sleep(time.Second * 1)
-		// 	q.InputOn <- true
-		// 	time.Sleep(time.Second * 3)
-		// 	q.InputOff <- true
-		// 	fmt.Printf("Q len: %v\n", q.Q.Len())
-		// }
 
-		initButtons(q)
+	//set up for the sensors:
+	go func() {
+
+		initPins(q)
+		q.InfeedSensor()
+		q.OutfeedSensor()
 	}()
 
 	go blinky()
 
-	// go func() {
-	// 	time.Sleep(time.Second * 4)
-	// 	for {
-	// 		time.Sleep(time.Second * 1)
-	// 		q.OutputOn <- true
-	// 		time.Sleep(time.Second * 3)
-	// 		q.OutputOff <- true
-	// 	}
-	// }()
+	//start the ticker for memStats:
+	stats := &runtime.MemStats{}
+	ticker := time.NewTicker(time.Second * 5)
+	defer ticker.Stop()
 
-	//start the ticker and attach to Q:
-	// ticker := time.NewTicker(time.Second * 3)
-	// defer ticker.Stop()
-
-	// q.Ticker = ticker
 	//set up queue receiver:
+	//now only used for periodic memStats and the KillFunc()
 	for {
 		select {
-		case <-q.InputOn:
-			go q.NewProcess()
-		case <-q.OutputOn:
-			go q.EndProcess()
-		// case <-q.ResetChan:
-		// go q.Reset()
-		//set a time bomb of 3 seconds so if the Q is greater than 1 and this is called, we kill.
-		// case <-q.Ticker.C:
-		// 	q.LenCheck()
+		case <-ticker.C:
+			runtime.ReadMemStats(stats)
+			fmt.Printf("Heap in use: %v\n", stats.HeapInuse)
 		case <-q.Kill:
 			q.KillFunc()
 		}
@@ -252,30 +224,17 @@ func main() {
 
 }
 
-// func (q Queue) LenCheck() {
-// 	//if this is called, we haven't seen sheets for 3 seconds and need to check if the Q is greater than zero.
-// 	//if it is greater than 0, it means we lost a sheet:
-// 	if q.Q.Len() > 0 {
-// 		fmt.Printf("SHEET STUCK IN TUNNEL, QLEN = %v\n", q.Q.Len())
-// 		q.Kill <- true
-// 	}
-// 	q.Ticker.Reset(time.Second * 3)
-// 	return
-//}
-
 func (q Queue) KillFunc() {
 
-	fmt.Println("KILL ACTIVE. SOFT RESET, PLEASE WAIT...")
+	fmt.Println("KILL ACTIVE.")
 	panic("KILL ME")
 	// q.EmptyQ()
 	//send kill signals
-	time.Sleep(time.Second * 5)
-	fmt.Printf("Q Len = %v, READY TO RUN\n", q.Q.Len())
 	return
 
 }
 
-func initButtons(q Queue) {
+func initPins(q Queue) {
 	//10 - 13
 	pin10 := machine.GPIO12
 	pin11 := machine.GPIO13
@@ -295,10 +254,10 @@ func initButtons(q Queue) {
 		if *lastState10 != *currState10 {
 			*lastState10 = p.Get()
 			if p.Get() == false {
-				fmt.Printf("pushed inputon\n")
+				// fmt.Printf("pushed inputon\n")
 				q.InputOn <- true
 			} else {
-				fmt.Printf("released inputon\n")
+				// fmt.Printf("released inputon\n")
 				q.InputOff <- true
 			}
 		}
@@ -311,37 +270,13 @@ func initButtons(q Queue) {
 		if *lastState11 != *currState11 {
 			*lastState11 = p.Get()
 			if p.Get() == false {
-				fmt.Printf("pushed output\n")
+				// fmt.Printf("pushed output\n")
 				q.OutputOn <- true
 			} else {
-				fmt.Printf("output released\n")
+				// fmt.Printf("output released\n")
 				q.OutputOff <- true
 			}
 		}
 	})
 
-	//RESET BUTTON
-	// pin12.SetInterrupt(machine.PinRising, func(p machine.Pin) {
-	// 	//when pushed:
-	// 	currState12 = p.Get()
-	// 	if lastState12 != currState12 {
-	// 		lastState12 = p.Get()
-	// 		q.ResetChan <- true
-	// 	}
-	// })
-
-	//BLANK BUTTON
-	pin13.SetInterrupt(PinToggle, func(p machine.Pin) {
-		//when pushed:
-		*currState13 = p.Get()
-		if *lastState13 != *currState13 {
-			*lastState13 = p.Get()
-			if p.Get() == false {
-				fmt.Printf("pushed 13\n")
-			} else {
-				fmt.Printf("released 13\n")
-			}
-		}
-
-	})
 }
